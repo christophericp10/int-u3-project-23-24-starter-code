d4392e39929262e1f67f3818f538665ebfb04a34
YOUR <christophericp10@gmail.com>
2026-06-12 13:20:39 -0400

    First-Game

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Declare variables below to save the different divs of your story.
-
-
-
-
+let storyOpening = document.querySelector(".story-opening");
+let optionOne = document.querySelector(".option-one");
+let optionTwo = document.querySelector(".option-two");
+let optionOneScreen = document.querySelector(".option-one-screen");
+let optionTwoScreen = document.querySelector(".option-two-screen");
+let optionOneEnd = document.querySelector(".option-one-end");
+let optionTwoEnd = document.querySelector(".option-two-end");
+let title = document.querySelector(".title");
+let wrapper = document.querySelector(".wrapper");
+let body = document.querySelector("body");
 
 
 
@@ -12,15 +18,29 @@
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+optionOne.addEventListener('click', function () {
+    optionOneScreen.style.display = "block";
+    storyOpening.style.display = "none";
+    optionTwo.style.display = "none";
+    optionOne.style.display  = "none";
+});
 
-// });
+optionOneScreen.addEventListener('keydown', function () {
+    optionOneEnd.style.display = "block";
+    optionOneScreen.style.display = "none";
+    title.innerHTML = "Spurs Win!";
+});
 
-// INSERT_VARIABLE.addEventListener('click', function(){
 
-// });
+optionTwo.addEventListener('click', function () {
+    optionTwoScreen.style.display = "block";
+    storyOpening.style.display = "none";
+    optionTwo.style.display = "none";
+    optionOne.style.display  = "none";
+});
 
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
+optionTwoScreen.addEventListener('keydown', function () {
+    optionTwoEnd.style.display = "block";
+    optionTwoScreen.style.display = "none";
+    title.innerHTML = "Knicks Win!";
+});
